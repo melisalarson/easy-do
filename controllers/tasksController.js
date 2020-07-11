7cebba3b684892d0ba08ffda353ceee9d6f68664
jimmyspann <jimmyleespann@outlook.com>
2020-07-11 18:30:22 -0400

    made task-card view and options on each card

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -4,15 +4,15 @@ const db = require('../models');
 
 const sampleData = 
   [
-    { name: 'Model folder -> Collaborator file',          collaborators: { name:'Melisa', img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '1 hour', stage: 'to-do' },
-    { name: 'Model folder -> Task file',                  collaborators: { name:'Melisa', img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '2 hour', stage: 'to-do' },
-    { name: 'Controllers folder -> task file',            collaborators: { name:'Melisa', img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '3 hour', stage: 'to-do' },
-    { name: 'Controllers folder -> collaborator file',    collaborators: { name:'Melisa', img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '4 hour', stage: 'in-progress' },
-    { name: 'Views folder -> index file',                 collaborators: { name:'Jimmy' , img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '5 hour', stage: 'in-progress' },
-    { name: 'Views folder -> tasks folder -> index file', collaborators: { name:'Jimmy' , img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '6 hour', stage: 'in-progress' },
-    { name: 'Views folder -> tasks folder -> new file',   collaborators: { name:'Jimmy' , img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '7 hour', stage: 'completed' },
-    { name: 'Views folder -> tasks folder -> edit file',  collaborators: { name:'Jimmy' , img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '8 hour', stage: 'completed' },
-    { name: 'Views folder -> tasks folder -> edit file',  collaborators: { name:'Jimmy' , img_url:'https://www.kindpng.com/imgv/hxibTwh_default-profile-hd-png-download/#gal_default-profile-hd-png-download_hxibTwh_4517876.png'}, completionTime: '9 hour', stage: 'completed' },
+    { name: 'Model folder -> Collaborator file',          collaborators: 'Melisa', completionTime: '1 hour', stage: 'to-do' },
+    { name: 'Model folder -> Task file',                  collaborators: 'Melisa', completionTime: '2 hour', stage: 'to-do' },
+    { name: 'Controllers folder -> task file',            collaborators: 'Melisa', completionTime: '3 hour', stage: 'to-do' },
+    { name: 'Controllers folder -> collaborator file',    collaborators: 'Melisa', completionTime: '4 hour', stage: 'in-progress' },
+    { name: 'Views folder -> index file',                 collaborators: 'Jimmy' , completionTime: '5 hour', stage: 'in-progress' },
+    { name: 'Views folder -> tasks folder -> index file', collaborators: 'Jimmy' , completionTime: '6 hour', stage: 'in-progress' },
+    { name: 'Views folder -> tasks folder -> new file',   collaborators: 'Jimmy' , completionTime: '7 hour', stage: 'completed' },
+    { name: 'Views folder -> tasks folder -> edit file',  collaborators: 'Jimmy' , completionTime: '8 hour', stage: 'completed' },
+    { name: 'Views folder -> tasks folder -> edit file',  collaborators: 'Jimmy' , completionTime: '9 hour', stage: 'completed' },
   ];
 
 
