b05032497fd9bcf43ab4aad3c8494cea90abfffd
melisalarson <larsonmelisa@gmail.com>
2020-07-10 23:27:10 -0700

    skeleton for tasks routes

--- controllers/collaboratorsController.js
+++ controllers/collaboratorsController.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../models');
+
+// 1)collab index route
+// 2)collab new route
+// 3)collab create route
+// 4)collab show route
+// 5)collab edit route
+// 6)collab update route
+// 7)collab destrow route
+
+
 module.exports = router;
--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,6 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/', (req, res) => { res.render('tasks/index') });
+
+// 1)task index route
+router.get('/', (req, res) => {
+  db.Task.find({}, (err, allTasks) => {
+    if (err) return console.log(err);
+    console.log(allTasks);
+    
+    res.render('tasks/index', { allTasks });
+  });
+});
+
+// 2)task new route
+router.get('/new', (req, res) => {
+  
+  res.render('tasks/new');
+});
+
+// 4)task show route
+router.get('/:id', (req, res) => {
+  db.Task.findById(req.params.id)
+    .populate({ path: 'collaborators' })
+    .exec((err, foundTask) => {
+      if (err) return console.log(err);
+      console.log(foundTask);
+      
+      res.render('tasks/show');
+    });
+});
+
+// 3)task create route
+router.post('/', (req, res) => {
+  db.Task.create(
+    req.body,
+    (err, newTask) => {
+      if (err) return console.log(err);
+      console.log(newTask);
+      
+      res.redirect('/tasks');
+  });
+});
+
+// 5)task edit route
+router.get('/:id/edit', (req, res) => {
+  db.Task.findById(
+    req.params.id,
+    (err, taskToEdit) => {
+      if (err) return console.log(err);
+      console.log(taskToEdit);
+      
+      res.render('tasks/edit', {foundTask})
+  });
+});
+
+// 6)task update route
+router.put('/:id', (req, res) => {
+  db.Task.findByIdAndUpdate(
+    req.params.id, 
+    req.body,
+    {new: true},
+    (err, taskToUpdate) => {
+      if (err) return console.log(err);
+      console.log(taskToUpdate);
+      
+      res.redirect('/tasks');
+  });
+});
+
+// 7)task destroy route
+router.delete('/:id', (req, res) => {
+  db.Task.findByIdDelete(
+    req.params.id,
+    (err, deletedTask) => {
+      if (err) return console.log(err);
+      console.log(deletedTask);
+      res.redirect('/tasks');
+    });
+});
+
 
 module.exports = router;
