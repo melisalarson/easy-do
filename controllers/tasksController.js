0e66aaf113ce70f65a73354d56a4d59a2ddbce67
jimmyspann <jimmyleespann@outlook.com>
2020-07-11 18:37:00 -0400

    returned taskControllers to normal

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -4,30 +4,16 @@ const db = require('../models');
 
 const sampleData = 
   [
-    { name: 'Model folder -> Collaborator file',          collaborators: 'Melisa', completionTime: '1 hour', stage: 'to-do' },
-    { name: 'Model folder -> Task file',                  collaborators: 'Melisa', completionTime: '2 hour', stage: 'to-do' },
-    { name: 'Controllers folder -> task file',            collaborators: 'Melisa', completionTime: '3 hour', stage: 'to-do' },
-    { name: 'Controllers folder -> collaborator file',    collaborators: 'Melisa', completionTime: '4 hour', stage: 'in-progress' },
-    { name: 'Views folder -> index file',                 collaborators: 'Jimmy' , completionTime: '5 hour', stage: 'in-progress' },
-    { name: 'Views folder -> tasks folder -> index file', collaborators: 'Jimmy' , completionTime: '6 hour', stage: 'in-progress' },
-    { name: 'Views folder -> tasks folder -> new file',   collaborators: 'Jimmy' , completionTime: '7 hour', stage: 'completed' },
-    { name: 'Views folder -> tasks folder -> edit file',  collaborators: 'Jimmy' , completionTime: '8 hour', stage: 'completed' },
-    { name: 'Views folder -> tasks folder -> edit file',  collaborators: 'Jimmy' , completionTime: '9 hour', stage: 'completed' },
+    { name: 'Model folder -> Collaborator file', collaborators: 'Melisa', completionTime: '1 hour', stage: 'to-do' },
+    { name: 'Model folder -> Task file', collaborators: 'Melisa', completionTime: '2 hour', stage: 'to-do' },
+    { name: 'Controllers folder -> task file', collaborators: 'Melisa', completionTime: '3 hour', stage: 'to-do' },
+    { name: 'Controllers folder -> collaborator file', collaborators: 'Melisa', completionTime: '4 hour', stage: 'to-do' },
+    { name: 'Views folder -> index file', collaborators: 'Jimmy', completionTime: '5 hour', stage: 'to-do' },
+    { name: 'Views folder -> tasks folder -> index file', collaborators: 'Jimmy', completionTime: '6 hour', stage: 'to-do' },
+    { name: 'Views folder -> tasks folder -> new file', collaborators: 'Jimmy', completionTime: '7 hour', stage: 'to-do' },
+    { name: 'Views folder -> tasks folder -> edit file', collaborators: 'Jimmy', completionTime: '8 hour', stage: 'to-do' },
+    { name: 'Views folder -> tasks folder -> edit file', collaborators: 'Jimmy', completionTime: '9 hour', stage: 'to-do' },
   ];
-
-
-// const sampleData = 
-//   [
-//     { name: 'Model folder -> Collaborator file', collaborators: 'Melisa', completionTime: '1 hour', stage: 'to-do' },
-//     { name: 'Model folder -> Task file', collaborators: 'Melisa', completionTime: '2 hour', stage: 'to-do' },
-//     { name: 'Controllers folder -> task file', collaborators: 'Melisa', completionTime: '3 hour', stage: 'to-do' },
-//     { name: 'Controllers folder -> collaborator file', collaborators: 'Melisa', completionTime: '4 hour', stage: 'to-do' },
-//     { name: 'Views folder -> index file', collaborators: 'Jimmy', completionTime: '5 hour', stage: 'to-do' },
-//     { name: 'Views folder -> tasks folder -> index file', collaborators: 'Jimmy', completionTime: '6 hour', stage: 'to-do' },
-//     { name: 'Views folder -> tasks folder -> new file', collaborators: 'Jimmy', completionTime: '7 hour', stage: 'to-do' },
-//     { name: 'Views folder -> tasks folder -> edit file', collaborators: 'Jimmy', completionTime: '8 hour', stage: 'to-do' },
-//     { name: 'Views folder -> tasks folder -> edit file', collaborators: 'Jimmy', completionTime: '9 hour', stage: 'to-do' },
-//   ];
 
 // 1)task index route
 router.get('/', (req, res) => {
