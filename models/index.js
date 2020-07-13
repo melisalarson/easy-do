8788add523edc0452a7d8c8cc808b5b1fbfd7f67
melisalarson <larsonmelisa@gmail.com>
2020-07-12 21:02:13 -0700

    switched to remote connection string

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const connectionString = ('mongodb://localhost:27017/easy-do-db');
-// const connectionString = 'mongodb://app:UPrIJaqCeZz5v71v@cluster0-shard-00-00.judrx.mongodb.net:27017,cluster0-shard-00-01.judrx.mongodb.net:27017,cluster0-shard-00-02.judrx.mongodb.net:27017/easy-do?ssl=true&replicaSet=atlas-ljx4vy-shard-0&authSource=admin&retryWrites=true&w=majority';
+// const connectionString = ('mongodb://localhost:27017/easy-do-db');
+const connectionString = 'mongodb://app:UPrIJaqCeZz5v71v@cluster0-shard-00-00.judrx.mongodb.net:27017,cluster0-shard-00-01.judrx.mongodb.net:27017,cluster0-shard-00-02.judrx.mongodb.net:27017/easy-do?ssl=true&replicaSet=atlas-ljx4vy-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
