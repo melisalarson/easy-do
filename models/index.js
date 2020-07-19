4de4a19de6bfedf60efb7176036f02a25b6ca3b4
melisalarson <larsonmelisa@gmail.com>
2020-07-18 19:22:24 -0700

    register login and logout all working properlly on the back end

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// localdb comment lines 5/6 and uncomment line 8 for remote db
 const MONGODB_URI = ('mongodb://localhost:27017/easy-do-db');
+mongoose.connect(MONGODB_URI, {
 
-mongoose.connect(/*process.env.*/MONGODB_URI, {
+// mongoose.connect(process.env.MONGODB_MJ, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
